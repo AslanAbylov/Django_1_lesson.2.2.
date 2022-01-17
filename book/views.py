1b1973944bb25c8ffac247bbd67aea1cb2a742ed
Aslan Abylov <aslanabylov@gmail.com>
2022-01-17 18:24:29 +0300

    Aslan_hv2

--- book/views.py
+++ book/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
 from book.models import Book
-from book import models
+from book import models, forms
 
 
 def get_books(request):
@@ -11,3 +12,16 @@ def get_books(request):
 def books_detail(request, id):
     books = get_object_or_404(models.Book, id=id)
     return render(request, 'books_detail.html', {'books': books})
+
+
+
+def add_book(request):
+    method = request.method
+    if method == 'POST':
+        form = forms.BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Book created')
+    else:
+        form = forms.BookForm()
+    return render(request, 'add_book.html', {'form': form})
