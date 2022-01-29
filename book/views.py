62ba38306c653920ca7280eaae641181af601d4e
Aslan Abylov <aslanabylov@gmail.com>
2022-01-29 21:44:16 +0300

    Aslan_hv3

--- book/views.py
+++ book/views.py
@@ -1,15 +1,31 @@
 from audioop import reverse
-
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.shortcuts import render
 from book.models import Book
 from book import models, forms
+from django.views import generic
+
+# class BookListView(generic.ListView):
+#     template_name = 'book_list.html'
+#     queryset = models.Book.objects.all()
+#
+#     def get_queryset(self):
+#         return models.Book.objects.all()
 
 
 def get_books(request):
     book = models.Book.objects.all()
     return render(request, 'book_list.html', {'book': book})
+
+
+# class BookDetailView(generic.DetailView):
+#     template_name = 'books_detail.html'
+#
+#     def get_object(self, **kwargs):
+#         book_id = self.kwargs.get("id")
+#         return get_object_or_404(models.Book, id=book_id)
+
 
 def books_detail(request, id):
     books = get_object_or_404(models.Book, id=id)
@@ -23,7 +39,8 @@ def add_book(request):
         form = forms.BookForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return HttpResponse('Book created')
+            # return HttpResponse('Book created')
+            return redirect(reverse('books:books_all'))
     else:
         form = forms.BookForm()
     return render(request, 'add_book.html', {'form': form})
@@ -36,7 +53,8 @@ def book_update(request, id):
         form = forms.BookForm(instance=book_object, data=request.POST)
         if form.is_valid():
             form.save()
-            return redirect(reverse("books:books_all"))
+            return HttpResponse("Book Updated Successfully")
+            # return redirect(reverse("books:books_all"))
     else:
         form = forms.BookForm(instance=book_object)
     return render(request, "book_update.html", {"form": form, "object": book_object})
@@ -45,6 +63,6 @@ def book_update(request, id):
 def book_delete(request, id):
     book_object = get_object_or_404(models.Book, id=id)
     book_object.delete()
-    return redirect(reverse("books:books_all"))
+    return HttpResponse('Show Deleted')
 
 
